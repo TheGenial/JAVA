2cae806748c528cec9fc13d04bbd5093bc57181d
TheGenial <arifulislam14.ict@gmail.com>
2018-12-16 17:20:15 +0600

    Reverse Commit

--- Core Java/Enum & AutoBoxing/src/Wrapper.java
+++
@@ -1,9 +0,0 @@
-public class Wrapper {
-    public static void main(String[] args) {
-        Integer iOb = new Integer(100); // boxing
-        System.out.println(iOb);
-
-        int i = iOb.intValue();
-        System.out.println(i + " " + iOb); //unboxing
-    }
-}
